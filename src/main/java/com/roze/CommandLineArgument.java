f029612d28628b32cf37e00dc73a6e30d54e982f
roze28 <firoze.hossain01@yahoo.com>
2022-04-20 16:30:13 +0600

    Command Line Argument

---
+++ src/main/java/com/roze/CommandLineArgument.java
@@ -0,0 +1,15 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * Command Line Argument
+ */
+public class CommandLineArgument {
+
+    public static void main(String[] args) {
+        for (String t : args) {
+            System.out.println(t);
+        }
+    }
+}
--- src/main/java/com/roze/PrintInteger.java
+++ src/main/java/com/roze/PrintInteger.java
@@ -9,7 +9,7 @@ public class PrintInteger {
 
     public static void main(String[] args) {
         int i;
-        for (i = 1; i <= 10; i++) {
+        for (i = 1; i <=10; i++) {
             System.out.println(i);
         }
     }
