8aedd0d71eb5abf5bf35e37918e4048a0a70c3e6
roze28 <firoze.hossain01@yahoo.com>
2022-04-20 16:10:07 +0600

    Simple Java Program

---
+++ src/main/java/com/roze/simpleProgram/SimpleJavaProgram.java
@@ -0,0 +1,12 @@
+package com.roze.simpleProgram;
+
+/**
+ *
+ * @author firoze
+ */
+public class SimpleJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java's World");
+    }
+}
