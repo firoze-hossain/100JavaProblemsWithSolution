c9573458037dcb972930d7706321bade9886b6ba
firoze <firoze@firoze>
2022-04-22 01:26:11 +0600

    If Else clause

---
+++ src/main/java/com/roze/IfClause10.java
@@ -0,0 +1,18 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * If Else clause
+ */
+public class IfClause10 {
+
+    public static void main(String[] args) {
+        boolean learning = false;
+        if (learning) {
+            System.out.println("Firoze is Passionate Java Programmer");
+        } else {
+            System.out.println("What are you doing here!!! ");
+        }
+    }
+}
