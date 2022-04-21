a600de06dc312fbd038e4640041b23bdf474ff11
firoze <firoze@firoze>
2022-04-22 01:07:08 +0600

    Add Two Large Numbers

---
+++ src/main/java/AddTwoLargeNumber9.java
@@ -0,0 +1,28 @@
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ *
+ * @author firoze 
+ * Add Two Large Numbers
+ */
+public class AddTwoLargeNumber9 {
+
+    public static void main(String[] args) {
+        String firstNum, secondNum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number: ");
+        firstNum = sc.nextLine();
+        System.out.println("Enter the Second Number: ");
+        secondNum = sc.nextLine();
+
+        BigInteger firstBigInteger = new BigInteger(firstNum);
+        BigInteger secondBigInteger = new BigInteger(secondNum);
+        BigInteger summation;
+        summation = firstBigInteger.add(secondBigInteger);
+        System.out.println("Summation of Two Large Number: " + summation);
+
+    }
+
+}
