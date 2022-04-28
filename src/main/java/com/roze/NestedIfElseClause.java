f2866fd832fbd0b34cafebc515a0f87b3ed40580
roze28 <firoze.hossain01@yahoo.com>
2022-04-28 22:41:15 +0600

    Nested If Else clause in java

---
+++ src/main/java/com/roze/NestedIfElseClause.java
@@ -0,0 +1,40 @@
+package com.roze;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author firoze
+ * Nested If Else clause in java
+ */
+public class NestedIfElseClause {
+
+    public static void main(String[] args) {
+        int obtainedMarks, passingMarks = 40;
+        char grade;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your obtained marks: ");
+        obtainedMarks = sc.nextInt();
+
+        if (obtainedMarks >= passingMarks) {
+
+            if (obtainedMarks >= 90) {
+                grade = 'A';
+            } else if (obtainedMarks >= 80) {
+                grade = 'B';
+
+            } else if (obtainedMarks >= 70) {
+                grade = 'C';
+            } else {
+                grade = 'D';
+
+            }
+            System.out.println("You pass the exam is grade: " + grade);
+        } else {
+            grade = 'F';
+            System.out.println("You failed and grade is: " + grade);
+        }
+
+    }
+
+}
