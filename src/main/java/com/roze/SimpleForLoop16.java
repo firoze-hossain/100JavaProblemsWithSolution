89120d5125260aa37abeea263b17dc8a4f3dea50
roze28 <firoze.hossain01@yahoo.com>
2022-05-02 00:34:48 +0600

    Simple For Loop Program in Java

---
+++ src/main/java/com/roze/SimpleForLoop16.java
@@ -0,0 +1,16 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * Simple For Loop Program in Java
+ */
+public class SimpleForLoop16 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
