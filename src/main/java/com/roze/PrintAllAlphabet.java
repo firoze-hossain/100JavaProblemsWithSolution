6bd770c4e70166bd27bde69d0d4fd71691628714
roze28 <firoze.hossain01@yahoo.com>
2022-05-06 01:15:12 +0600

    Print all alphabet using for loop Program in java

---
+++ src/main/java/com/roze/PrintAllAlphabet.java
@@ -0,0 +1,15 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * Print all alphabet using for loop Program in java
+ */
+public class PrintAllAlphabet {
+
+    public static void main(String[] args) {
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.println(ch);
+        }
+    }
+}
