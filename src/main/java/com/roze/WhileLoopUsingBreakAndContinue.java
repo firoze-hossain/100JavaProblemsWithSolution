36aa9bef16c46fb4b16dba0a93b78a22a48a6476
roze28 <firoze.hossain01@yahoo.com>
2022-05-06 01:00:22 +0600

    While loop using break and continue Program in java

---
+++ src/main/java/com/roze/WhileLoopUsingBreakAndContinue.java
@@ -0,0 +1,27 @@
+package com.roze;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author firoze 
+ * While loop using break and continue Program in java
+ */
+public class WhileLoopUsingBreakAndContinue {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Input an integer: ");
+            int num = sc.nextInt();
+            if (num != 0) {
+                System.out.println("You entered: " + num);
+                continue;
+            } else {
+                break;
+            }
+
+        }
+    }
+
+}
