b8b4ba23364f45ecd63bdda983eb44c52b6ecba2
roze28 <firoze.hossain01@yahoo.com>
2022-06-19 22:09:46 +0600

    How to compare 2 string in java Program

---
+++ src/main/java/com/roze/CompareTwoString35.java
@@ -0,0 +1,20 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * How to compare 2 string in java Program
+ */
+public class CompareTwoString35 {
+
+    public static void main(String[] args) {
+        String str1 = "Firoze";
+        String str2 = "Firoze";
+        String str3 = "Hossain";
+        String str4 = "Roze";
+        System.out.println(str1.compareTo(str2));
+        System.out.println(str1.compareTo(str3));
+        System.out.println(str1.compareTo(str4));
+    }
+
+}
