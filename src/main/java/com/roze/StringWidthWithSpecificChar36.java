83dd5c6105f5c65e3fd993b52052255f7f0590ad
roze28 <firoze.hossain01@yahoo.com>
2022-06-19 22:15:53 +0600

    How to get string width with specific char in java Program

---
+++ src/main/java/com/roze/StringWidthWithSpecificChar36.java
@@ -0,0 +1,18 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to get string width with specific char in java Program
+ */
+public class StringWidthWithSpecificChar36 {
+
+    public static void main(String[] args) {
+        String s1 = "Java By FirozeHossain";
+        System.out.println(s1.endsWith("n"));
+        System.out.println(s1.endsWith("Hosain"));
+        System.out.println(s1.endsWith("ain"));
+        
+    }
+    
+}
