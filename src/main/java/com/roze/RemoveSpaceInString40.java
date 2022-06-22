1b6d95dfd39c98ed17c8cebe6706a8faf8f8c4ea
roze28 <firoze.hossain01@yahoo.com>
2022-06-22 19:18:37 +0600

    How to remove space in string both end in java Program

---
+++ src/main/java/com/roze/RemoveSpaceInString40.java
@@ -0,0 +1,16 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * How to remove space in string both end in java Program
+ */
+public class RemoveSpaceInString40 {
+
+    public static void main(String[] args) {
+        String str = " Hello Firoze  ";
+        System.out.println(str + "Hossain");
+        System.out.println(str.trim() + "Hossain");
+    }
+
+}
