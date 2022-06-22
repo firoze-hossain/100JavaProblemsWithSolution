6052b65da6b327ff8af3edafc4d16d21c1454a81
roze28 <firoze.hossain01@yahoo.com>
2022-06-22 19:02:49 +0600

    How to replace string with another string in java Program

---
+++ src/main/java/com/roze/ReplaceStringWithAnotherString.java
@@ -0,0 +1,15 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * How to replace string with another string in java Program
+ */
+public class ReplaceStringWithAnotherString {
+
+    public static void main(String[] args) {
+        String str = "I am Firoze. I am C Developer. I am Spring Boot Developer";
+        System.out.println(str.replaceAll("am", "was"));
+    }
+
+}
