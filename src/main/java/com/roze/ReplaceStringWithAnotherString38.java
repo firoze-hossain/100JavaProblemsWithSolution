f1be8baaa5818b178433d055f01d7ff1a859b2c2
roze28 <firoze.hossain01@yahoo.com>
2022-06-22 19:03:53 +0600

    renamed

--- src/main/java/com/roze/ReplaceStringWithAnotherString38.java
+++ src/main/java/com/roze/ReplaceStringWithAnotherString38.java
@@ -5,7 +5,7 @@ package com.roze;
  * @author firoze 
  * How to replace string with another string in java Program
  */
-public class ReplaceStringWithAnotherString {
+public class ReplaceStringWithAnotherString38 {
 
     public static void main(String[] args) {
         String str = "I am Firoze. I am C Developer. I am Spring Boot Developer";
