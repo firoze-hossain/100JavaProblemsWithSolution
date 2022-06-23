5efed5654bddbf70ee696869b1f9e7b86d1bf14e
roze28 <firoze.hossain01@yahoo.com>
2022-06-23 12:36:06 +0600

    How to convert all char in string lower case in java Program

---
+++ src/main/java/com/roze/ConvertIntoLowerCase41.java
@@ -0,0 +1,15 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * How to convert all char in string lower case in java Program
+ */
+public class ConvertIntoLowerCase41 {
+
+    public static void main(String[] args) {
+        String str = "THIS is MD. FIROZE HOSSAIN";
+        String lowerCase = str.toLowerCase();
+        System.out.println(lowerCase);
+    }
+}
