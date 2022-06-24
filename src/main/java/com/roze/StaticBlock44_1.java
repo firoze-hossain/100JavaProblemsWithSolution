6a67e739aab634f0d96952bc9592a8335b74cf41
roze28 <firoze.hossain01@yahoo.com>
2022-06-24 21:17:56 +0600

    How Static block working in java Program for finding out os property

---
+++ src/main/java/com/roze/StaticBlock44_1.java
@@ -0,0 +1,22 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze 
+ * How Static block working in java Program
+ *
+ */
+public class StaticBlock44_1 {
+
+    public static void main(String[] args) {
+        System.out.println("You are using Ubuntu 20.04.4 LTS");
+    }
+
+    static {
+        String osName = System.getProperty("os.name");
+        System.out.println("OS Name: " + osName);
+        String osVersion = System.getProperty("os.version");
+        System.out.println("OS Version: " + osVersion);
+
+    }
+}
