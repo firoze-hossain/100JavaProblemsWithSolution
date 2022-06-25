d692d8e22f8d34774f08ba7c8ef27a89be02301c
roze28 <firoze.hossain01@yahoo.com>
2022-06-25 22:04:58 +0600

    How to create constructor in java Program

---
+++ src/main/java/CreateConstructor.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author firoze
+ * How to create constructor in java Program
+ */
+public class CreateConstructor {
+
+    public CreateConstructor() {
+        System.out.println("Constructor method created");
+    }
+
+    public static void main(String[] args) {
+        CreateConstructor cons = new CreateConstructor();
+    }
+
+}
