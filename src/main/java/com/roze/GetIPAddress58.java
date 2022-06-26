3bf23c7f74da7feacf7ba85805ba5fb44ea2126d
roze28 <firoze.hossain01@yahoo.com>
2022-06-26 16:10:36 +0600

    How to get own IP Address in java Program

---
+++ src/main/java/com/roze/GetIPAddress58.java
@@ -0,0 +1,15 @@
+package com.roze;
+
+import java.net.InetAddress;
+
+/**
+ *
+ * @author firoze
+ * How to get own IP Address in java Program
+ */
+public class GetIPAddress58 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(InetAddress.getLocalHost());
+    }
+}
