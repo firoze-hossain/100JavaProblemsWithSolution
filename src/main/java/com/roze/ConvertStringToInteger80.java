d3395229814e9286bfae70522d12561382daffb0
roze28 <firoze.hossain01@yahoo.com>
2022-07-01 16:28:15 +0600

    How to convert string to integer in java program

---
+++ src/main/java/com/roze/ConvertStringToInteger80.java
@@ -0,0 +1,17 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to convert string to integer in java program
+ */
+public class ConvertStringToInteger80 {
+
+    public static void main(String[] args) {
+        String str = "400";
+        int num = Integer.parseInt(str);
+        System.out.println("String concatenation = " + (100 + str));
+        System.out.println("Integer addition = " + (100 + num));
+    }
+
+}
