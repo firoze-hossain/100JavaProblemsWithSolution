c91f51139e51cd0c8ca484a6c1db914ec3e1278a
roze28 <firoze.hossain01@yahoo.com>
2022-07-02 10:36:02 +0600

    How to convert string to double in java program

---
+++ src/main/java/com/roze/ConvertStringToDouble84.java
@@ -0,0 +1,16 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to convert string to double in java program
+ */
+public class ConvertStringToDouble84 {
+
+    public static void main(String[] args) {
+        String str = "36.12";
+        double d = Double.parseDouble(str);
+        System.out.println("Double value is = " + d);
+    }
+
+}
