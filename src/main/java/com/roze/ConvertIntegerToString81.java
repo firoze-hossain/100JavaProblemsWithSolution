68f287504c5fd519cea2c1fb9211de7dfef3da1b
roze28 <firoze.hossain01@yahoo.com>
2022-07-02 10:24:35 +0600

    How to convert integer to string in java program

---
+++ src/main/java/com/roze/ConvertIntegerToString81.java
@@ -0,0 +1,17 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to convert integer to string in java program
+ */
+public class ConvertIntegerToString81 {
+
+    public static void main(String[] args) {
+        int num = 300;
+        String str = String.valueOf(num);
+        System.out.println(num + 100);
+        System.out.println(str + 100);
+    }
+
+}
