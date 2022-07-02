7424c9074227df9e8094b0bcb708979591f337f4
roze28 <firoze.hossain01@yahoo.com>
2022-07-02 10:28:50 +0600

    How to convert string to long in java

---
+++ src/main/java/com/roze/ConvertStringToLong82.java
@@ -0,0 +1,16 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to convert string to long in java
+ */
+public class ConvertStringToLong82 {
+
+    public static void main(String[] args) {
+        String str = "123564008";
+        long l = Long.parseLong(str);
+        System.out.println("The Long value is = " + l);
+    }
+
+}
