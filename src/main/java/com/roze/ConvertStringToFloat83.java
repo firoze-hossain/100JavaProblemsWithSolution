f1f41abab6cc2f423bba1e9d086c1db4c80a7524
roze28 <firoze.hossain01@yahoo.com>
2022-07-02 10:32:50 +0600

    How to convert string to float in java

---
+++ src/main/java/com/roze/ConvertStringToFloat83.java
@@ -0,0 +1,16 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to convert string to float in java
+ */
+public class ConvertStringToFloat83 {
+
+    public static void main(String[] args) {
+        String str = "23.16";
+        float f = Float.parseFloat(str);
+        System.out.println("The Float Value is = " + f);
+    }
+
+}
