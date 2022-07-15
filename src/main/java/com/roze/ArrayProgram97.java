7ce3ea4c35f4208f8cb997be099baa31009fe75d
roze28 <firoze.hossain01@yahoo.com>
2022-07-15 10:09:10 +0600

    How to create Array program in java

---
+++ src/main/java/com/roze/ArrayProgram97.java
@@ -0,0 +1,22 @@
+package com.roze;
+
+/**
+ *
+ * @author firoze
+ * How to create Array program in java
+ */
+public class ArrayProgram97 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 5;
+        arr[2] = 12;
+        arr[3] = 5;
+        arr[4] = 9;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
